060077ba817606e1fc9badee8069c730f4f02a68
Shaikh <firoz.md@atos.net>
2020-03-09 14:53:10 +0530

    first commit

--- app/src/main/java/com/example/startactivityforresultdemo/MainActivity.java
+++ app/src/main/java/com/example/startactivityforresultdemo/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.startactivityforresultdemo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView1;
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView1 = findViewById(R.id.textView1);
+        button1 = findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivityForResult(intent, 2);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==2){
+            String message = data != null ? data.getStringExtra("MESSAGE") : null;
+            textView1.setText(message);
+        }
     }
 }
